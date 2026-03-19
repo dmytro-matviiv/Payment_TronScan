ae18028f70e4f11df4b8fed19eac3a8c31245e6c
dmytro-matviiv <matvidima83@gmail.com>
2026-03-19 10:45:31 +0200

    New adres payment

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID", "@payment_trc20_001")  # 
 
 # Tronscan API Configuration
 TRONSCAN_API_TOKEN = os.getenv("TRONSCAN_API_TOKEN", "3bfa787b-22a1-4c79-a2f5-b46dc062ee9f")
-TRON_ADDRESS = os.getenv("TRON_ADDRESS", "TCKV8GCJcEzQWYi8c3yFGPvMa1UkUDYZ57")
+TRON_ADDRESS = os.getenv("TRON_ADDRESS", "TYY4QoQ8nA4geSoYpueQ14DLFnHhYBKCiR")
 
 # Monitoring Configuration
 CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", "30"))  # секунди між перевірками
