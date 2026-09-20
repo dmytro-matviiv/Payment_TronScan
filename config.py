8558f7c7af070e62ff60617ae88e1f1b0096cabe
dmytro-matviiv <matvidima83@gmail.com>
2026-09-20 13:11:47 +0300

    API Token + new chanel

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Telegram Bot Configuration
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8456055614:AAFeuIrPgQKDdfl_e9ULHi1oAJimxkaeLWM")
-TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID", "@payment_trc20_001")  # Username каналу
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8867231877:AAH6T6SKHNo7fTpACCyfWlr4RrLTBv1ai5E")
+TELEGRAM_CHANNEL_ID = os.getenv("TELEGRAM_CHANNEL_ID", "-1003903485012")  # Username каналу
 
 # Tronscan API Configuration
 TRONSCAN_API_TOKEN = os.getenv("TRONSCAN_API_TOKEN", "3bfa787b-22a1-4c79-a2f5-b46dc062ee9f")
